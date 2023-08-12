97a319b65c886c41d754418ac02552305539d13b
Justin Rodino <justin@rodino.dev>
2023-08-11 22:16:07 -0400

    Initial commit - framework of files

---
+++ tests/ImageTest.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace dutchie027\Test\PhpExif;
+
+use dutchie027\PhpExif\Image;
+
+use PHPUnit\Framework\TestCase;
+
+final class ImageTest extends TestCase
+{
+}
