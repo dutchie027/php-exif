29ac866f3f2f357b44dd9b24b0c310f2ed65920e
Justin Rodino <justin@rodino.dev>
2023-08-11 22:16:27 -0400

    Initial Commit - Framework

---
+++ src/Image.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * @version 0.1
+ *
+ * @license http://www.opensource.org/licenses/mit-license.php MIT
+ *
+ * @see     https://github.com/dutche027/php-exif
+ * @see     https://packagist.org/packages/dutchie027/phpexif
+ */
+
+namespace dutchie027\PhpExif;
+
+class Image
+{
+}
---
+++ tests/ExifTest.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace dutchie027\Test\PhpExif;
+
+use dutchie027\PhpExif\Exif;
+
+use PHPUnit\Framework\TestCase;
+
+final class ExifTest extends TestCase
+{
+}
